dde247a0e95c1b280f0c615f032b90257978d72c
Eward205204 <khoa123kd56cn@gmail.com>
2025-01-11 23:21:48 +0700

    first commit

---
+++ Bai1/index.js
@@ -0,0 +1,61 @@
+const $ = document.getElementById.bind(document);
+const inputPoint = $("input-point");
+const point1 = $("input-point-1");
+const point2 = $("input-point-2");
+const point3 = $("input-point-3");
+const priorityArea = $("priority-area");
+const priorityObj = $("priority-obj");
+
+const totalPoints = (area) => (obj) => (point1, point2, point3) => {
+  return point1 + point2 + point3 + area + obj;
+};
+
+const getAreaPoint = (area) => {
+  switch (area) {
+    case "A":
+      return 2;
+    case "B":
+      return 1;
+    case "C":
+      return 0.5;
+    default:
+      return 0;
+  }
+};
+
+const getObjPoint = (obj) => {
+  switch (obj) {
+    case "1":
+      return 2.5;
+    case "2":
+      return 1.5;
+    case "3":
+      return 1;
+    default:
+      return 0;
+  }
+};
+
+const handleSubmit = () => {
+  const calculateTotalPoints = totalPoints(getAreaPoint(priorityArea.value))(
+    getObjPoint(priorityObj.value)
+  )(
+    parseFloat(point1.value),
+    parseFloat(point2.value),
+    parseFloat(point3.value)
+  );
+
+  if (isNaN(calculateTotalPoints)) {
+    document.getElementById("result").innerHTML = `Vui lòng nhập đúng số điểm`;
+    return;
+  }
+
+  if (calculateTotalPoints >= parseFloat(inputPoint.value)) {
+    document.getElementById("result").innerHTML =
+      `Chúc mừng bạn đã đậu với số điểm là ${calculateTotalPoints}`;
+  } else {
+    document.getElementById("result").innerHTML =
+      `Rất tiếc bạn đã rớt với số điểm là ${calculateTotalPoints}`;
+  }
+};
+document.getElementById("submit").addEventListener("click", handleSubmit);
---
+++ Bai2/index.js
@@ -0,0 +1,40 @@
+const $ = document.getElementById.bind(document);
+const incomeInput = $("#income");
+const dependentsInput = $("#dependents");
+const nameInput = $("#name");
+
+const getTaxableIncome = (income) => (dependents) => {
+  return income - 4 - dependents * 1.6;
+};
+
+const getTaxRate = (taxableIncome) => {
+  if (taxableIncome <= 60) return 5;
+  if (taxableIncome <= 120) return 10;
+  if (taxableIncome <= 210) return 15;
+  if (taxableIncome <= 384) return 20;
+  if (taxableIncome <= 624) return 25;
+  if (taxableIncome <= 960) return 30;
+  return 35;
+};
+
+const calculateTax = (taxableIncome) => (taxRate) => {
+  return (taxableIncome * taxRate) / 100;
+};
+
+const handleCalculateTax = () => {
+  const taxableIncome = getTaxableIncome(parseFloat(incomeInput.value))(
+    parseInt(dependentsInput.value)
+  );
+  const taxRate = getTaxRate(taxableIncome);
+  const taxAmount = calculateTax(taxableIncome)(taxRate);
+
+  if (isNaN(taxAmount)) {
+    $("#result").innerHTML = `Vui lòng nhập đúng thông tin`;
+    return;
+  }
+
+  $("#result").innerHTML =
+    `${nameInput.value} phải trả thuế là: ${taxAmount.toFixed(2)} triệu đồng.`;
+};
+
+$("#calculate-tax").addEventListener("click", handleCalculateTax);
